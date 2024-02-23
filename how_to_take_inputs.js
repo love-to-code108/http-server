431c239b88cb7332753e141e9a01ffd8a7a5d1b1
love-to-code108 <love.to.code108@gmail.com>
2024-02-23 18:23:14 +0530

    i started making the http-server

--- how_to_take_inputs.js
+++ how_to_take_inputs.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const port = 5173;
 
 app.get("/",(req,res) => {
     const n = req.query.n;
+    console.log(res)
     res.send(req.toString());
     // res.send(n.toString());
 })
 
-app.listen("3000");
+app.listen(port , () => {
+    console.log("The server has been started on the port",port);
+});
