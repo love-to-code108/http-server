dcd40e6686526e8b2bdfd051bcd6ec9aa4abf2d2
love-to-code108 <love.to.code108@gmail.com>
2024-02-23 19:38:06 +0530

    making the thing that harkirat made i think i kinda understand whats happening here

--- how_to_take_inputs.js
+++ how_to_take_inputs.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 5173;
+const user = [{
+    name:"Biswanath",
+    kidney:[
+        {
+            statusOfKidney:true
+        },
+        {
+            statusOfKidney:false
+        }
+    ]
+}]
+
+// I DONT KNOW WHATS HAPPENING BUT BASICALLY "express.json()" ALLOWS YOU TO ACESS THE BODY OF THE "req" ELEMENT
+app.use(express.json())
+
 
 app.get("/",(req,res) => {
     const n = req.query.n;
     console.log(req.header);
     // res.send(req.toString());
     res.send("Hello world");
+})
+
+// THE POST REQUEST
+app.post("/" , (req,res) => {
+    const a = req.body.a
+    res.send(a.toString());
 })
 
 app.listen(port , () => {
